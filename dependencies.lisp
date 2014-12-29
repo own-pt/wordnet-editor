73fe57a0aa2080195cf1c29bce329ba0113a1aa9
Alexandre Rademaker <arademaker@gmail.com>
2014-12-29 19:11:56 -0200

    error in the dependencies

--- dependencies.lisp
+++ dependencies.lisp
@@ -3,3 +3,4 @@
 
 (require :aserve)
 (require :webactions)
+
--- packages.lisp
+++ packages.lisp
@@ -1,7 +1,7 @@
 ;; (C) 2013 IBM Corporation
 ;; Author: Alexandre Rademaker
 
-
 (defpackage #:wordnet
   (:use #:cl :db.agraph :prolog :db.agraph.sparql
-	:net.uri :net.aserve :net.html.generator :net.aserve.client))
+	:net.uri :net.aserve :net.html.generator
+	:net.aserve.client))
