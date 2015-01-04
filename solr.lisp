f7fff7a83efdae438c94bc6da0fcbcd1a0446cb8
Alexandre Rademaker <arademaker@gmail.com>
2015-01-04 17:07:56 -0200

    loading data into solr

--- solr.lisp
+++ solr.lisp
@@ -26,7 +26,7 @@
       (do ((res (or data (list (cons :id addr))))
 	   (a-triple (cursor-next-row query) (cursor-next-row query)))
 	  ((null a-triple)
-	   (if words (push (cons (adj-name "word") words) res) res))
+	   (if words (push (cons (intern (adj-name "word") :keyword) words) res) res))
 	(cond
 	  ((part= (predicate a-triple) !owl:sameAs)
 	   (setf res (synset-to-alist addr :suffix "br" :data res :plan plan)))
@@ -37,7 +37,47 @@
 	     (push (cons key val) res))))))))
 
 
-(defun load-solr (blocksize)
+(defun nomlex-to-alist (addr)
+  (labels ((tostr (apart regex replacement)
+	     (multiple-value-bind (val type extra)
+		 (upi->value apart)
+	       (declare (ignore val extra))
+	       (if (equal 0 type)
+		   (cl-ppcre:regex-replace regex (part->string apart :format :concise) replacement)
+		   (cl-ppcre:regex-replace regex (part->value apart) replacement)))))
+    (let ((query (get-triples :s addr)))
+      (do ((res (list (cons :id (part->string addr :format :concise))))
+	   (a-triple (cursor-next-row query)
+		     (cursor-next-row query)))
+	  ((null a-triple)
+	   res)
+	(let ((key (intern (tostr (predicate a-triple) ":" "_") :keyword)))
+	  (if (member (predicate a-triple) '(!nomlex:noun !nomlex:verb) :test #'part=)
+	      (let ((val (object (get-triple :s (object a-triple) :p !wn30:lexicalForm))))
+		(push (cons key (part->value val)) res))
+	      (push (cons key (tostr (object a-triple) "^nomlex:" "")) res)))))))
+
+
+(defun load-nomlex-solr (blocksize)
+  (let* ((current 0) 
+	 (total 0)
+	 (block-tmp nil)
+	 (a-triple (get-triples :p !rdf:type :o !nomlex:Nominalization))) 
+    (dolist (p objs)
+      (let ((id (car a-triple)))
+	(format *debug-io* "Processing ~a [~a/~a ~a]~%" id current blocksize total)
+	(push (remove-duplicates (nomlex-to-alist id) :test #'equal) block-tmp)
+	(setf current (1+ current))
+	(if (> current blocksize)
+	    (progn
+	      (solr:solr-add* *solr* block-tmp :commit t)
+	      (setf total (+ total current)
+		    current 0
+		    block-tmp nil)))))
+    (solr:solr-add* *solr* block-tmp :commit t)))
+
+
+(defun load-synsets-solr (blocksize)
   (let* ((current 0) 
 	 (total 0)
 	 (block-tmp nil)
