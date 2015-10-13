0b69e7fe6aa6defe5717ab1d5ba8f90d39c8e4cd
Fabricio Rosario <fcbrbr@gmail.com>
2015-10-13 10:04:03 -0300

    Fixing layout.

--- morphosemantic-links.lisp
+++ morphosemantic-links.lisp
@@ -4,17 +4,20 @@
 
 (defun get-synset-words (query synset)
   (mapcar (lambda (w) (part->value (car w))) 
-          (sparql:run-sparql query 
-                             :with-variables `((?synset . ,synset)) ;;(list (cons '?synset  synset))
-                             :engine :sparql-1.1 
-                             :results-format :lists)))
+          (sparql:run-sparql 
+           query 
+           :with-variables `((?synset . ,synset))
+           :engine :sparql-1.1 
+           :results-format :lists)))
 
 (defun generate-list-of-morphosemantic-links-to-delete ()
   (let ((rows (run-query-as-list "morphosemantic-links.sparql"))
         (cnt 0))
-    (with-open-file (out "/tmp/morphosemantic-links-pt.txt" :direction :output :if-exists :supersede)
+    (with-open-file (out "/tmp/morphosemantic-links-pt.txt" 
+                         :direction :output :if-exists :supersede)
       (dolist (rr rows)
-        (destructuring-bind (w1 w2 relation synsetId1 synsetId2 gloss1 gloss2 s1 s2) rr
+        (destructuring-bind (w1 w2 relation synsetId1 synsetId2 
+                                gloss1 gloss2 s1 s2) rr
           (incf cnt)
           (when (member cnt *to-delete*)
             (format out "~a ~a ~a~%" s1 relation s2)))))))
@@ -26,7 +29,8 @@
     (with-open-file (out "/tmp/morphosemantic-links-pt.html" :direction :output :if-exists :supersede)
       (format out "<html><header><META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></header><body><h1>Morphosemantic links PT.</h1><table>")
       (dolist (rr rows)
-        (destructuring-bind (w1 w2 relation synsetId1 synsetId2 gloss1 gloss2) rr
+        (destructuring-bind (w1 w2 relation 
+                                synsetId1 synsetId2 gloss1 gloss2) rr
           (incf cnt)
           (format out "<ul>")
           (format out "<li>[~a] <b>~a</b> &rArr; <i>~a</i> &rArr; <b>~a</b>~%"
@@ -46,10 +50,13 @@
 (defun generate-missing-nouns-html-report ()
   (let ((rows (run-query-as-list "missing-nouns.sparql"))
         (synset-words-query (sparql:parse-sparql (query-string "synset-words.sparql"))))
-    (with-open-file (out "/tmp/morpholinks-nouns.html" :direction :output :if-exists :supersede)
+    (with-open-file (out "/tmp/morpholinks-nouns.html" 
+                         :direction :output :if-exists :supersede)
       (format out "<html><header><META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></header><body><h1>Morpholinks sem tradu&ccedil;&atilde;o para o substantivo.</h1><table>")
       (dolist (rr rows)
-        (destructuring-bind (nomlex synsetId1 synsetId2 ptSynset1 ptSynset2 nomlex-verb nomlex-noun relation gloss1 gloss2 en-word1 en-word2) rr
+        (destructuring-bind (nomlex synsetId1 synsetId2 ptSynset1 ptSynset2 
+                                    nomlex-verb nomlex-noun relation 
+                                    gloss1 gloss2 en-word1 en-word2) rr
           (format out "<ul>~%")
           (format out "<li><b>~a</b> &rArr; <i>~a</i> &rArr; <b>~a</b> | <b>~a</b> &rArr; ~a</li>" 
                   (upi->value en-word1)
@@ -95,7 +102,9 @@
     (with-open-file (out "/tmp/morpholinks-nouns-heuristic-1.html" :direction :output :if-exists :supersede)
       (format out "<html><header><META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></header><body><h1>Morpholinks sem tradu&ccedil;&atilde;o para o substantivo (er/or - or/nte).</h1><table>")
       (dolist (rr rows)
-        (destructuring-bind (nomlex synsetId1 synsetId2 ptSynset1 ptSynset2 nomlex-verb nomlex-noun relation gloss1 gloss2 en-word1 en-word2) rr
+        (destructuring-bind (nomlex synsetId1 synsetId2 ptSynset1 ptSynset2
+                                    nomlex-verb nomlex-noun relation
+                                    gloss1 gloss2 en-word1 en-word2) rr
           (when (check-heuristic (upi->value en-word2) (upi->value nomlex-noun))
             (format out "<ul>~%")
             (format out "<li><b>~a</b> &rArr; <i>~a</i> &rArr; <b>~a</b> | <b>~a</b> &rArr; ~a</li>" 
@@ -127,11 +136,15 @@
 
 (defun generate-missing-verbs-html-report ()
   (let ((rows (run-query-as-list "missing-verbs.sparql"))
-        (synset-words-query (sparql:parse-sparql (query-string "synset-words.sparql"))))
-    (with-open-file (out "/tmp/morpholinks-verbs.html" :direction :output :if-exists :supersede)
+        (synset-words-query
+         (sparql:parse-sparql (query-string "synset-words.sparql"))))
+    (with-open-file (out "/tmp/morpholinks-verbs.html" 
+                         :direction :output :if-exists :supersede)
       (format out "<html><header><META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></header><body><h1>Morpholinks sem tradu&ccedil;&atilde;o para o verbo.</h1><table>")
       (dolist (rr rows)
-        (destructuring-bind (nomlex synsetId1 synsetId2 ptSynset1 ptSynset2 nomlex-verb nomlex-noun relation gloss1 gloss2 en-word1 en-word2) rr
+        (destructuring-bind (nomlex synsetId1 synsetId2 ptSynset1 ptSynset2 
+                                    nomlex-verb nomlex-noun relation
+                                    gloss1 gloss2 en-word1 en-word2) rr
           (format out "<ul>~%")
           (format out "<li><b>~a</b> &rArr; <i>~a</i> &rArr; <b>~a</b> | ~a &rArr; <b>~a</b></li>" 
                   (upi->value en-word1)
