35edd02ee0b7c67725acff8018c0c3581b43c49d
Fabricio Chalub <fcbrbr@gmail.com>
2015-10-17 14:03:16 -0300

    Removed obsolete comment.

--- wordsenses.lisp
+++ wordsenses.lisp
@@ -1,28 +1,6 @@
 
 (in-package :wordnet)
 
-;;; Initially, OpenWordnet-PT was in an inconsitent state where some synsets
-;;; had "containsWordSense" relations with blank nodes AND regular nodes.
-;;; To avoid any name conflict, we simply opted to rename ALL objects (regardless of
-;;; whether they are blank or not) to "tmp-wordsense-<synset-id>-<number>" and then
-;;; rename ALL these new nodes to its final value "wordsense-<synset-id>-<number>".
-;;;
-;;; So, given an inconsistent OpenWordnet-PT, the following methods need to be called
-;;; in the correct order:
-;;;
-;;; (process-all-blank-wordsenses)
-;;; 
-;;; at this point all synsets will have "containsWordSense" pointing
-;;; to "tmp-wordsense-xxx-yyy" nodes
-;;;
-;;; (rename-wordsenses)
-;;;
-;;; at this point all "tmp-wordsense-*" will be renamed to "wordsense-*".
-;;;
-;;; Since there was no information about the order of words in the
-;;; first place, it doesn't matter for these methods the final order
-;;; of the words.
-
 (defun make-new-wordsense-id (synset)
   (labels ((new-part (synset n)
 	     (resource (cl-ppcre:regex-replace "synset-([0-9]{8}-[anvs])"
